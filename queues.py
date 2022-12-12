185eb3afb3cb04c0354f54801710c33d20eaba32
SenyoGela <senogelamariconb@gmail.com>
2022-12-12 22:47:49 +0800

    add elements argument in init method

--- queues.py
+++ queues.py
@@ -4,7 +4,7 @@ from collections import deque
 
 class Queue:
     def __init__(self, *elements):
-        self._elements = deque()
+        self._elements = deque(elements)
 
     def __len__(self):
         return len(self._elements)
