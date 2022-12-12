5081174c3ac512a3c9d1137eb81938135234c970
SenyoGela <senogelamariconb@gmail.com>
2022-12-12 14:07:46 +0800

    add length and iterable method in class queue

--- queues.py
+++ queues.py
@@ -6,7 +6,17 @@ class Queue:
     def __init__(self):
         self._elements = deque()
 
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
     def enqueue(self, element):
         self._elements.append(element)
+
     def dequeue(self):
         return self._elements.popleft()
+
+
