58f6c94932ff99a32640c0609d0d62cee735b3c9
SenyoGela <senogelamariconb@gmail.com>
2022-12-14 01:33:00 +0800

    add run method

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -137,6 +137,12 @@ class Producer(Worker):
         super().__init__(speed, buffer)
         self.products = products
 
+    def run(self):
+        while True:
+            self.product = choice(self.products)
+            self.simulate_work()
+            self.buffer.put(self.product)
+            self.simulate_idle()
 
 
 
