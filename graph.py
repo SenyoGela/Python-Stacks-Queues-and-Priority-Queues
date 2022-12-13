a76be4953bf3c97d926e45a288f6a049eea5957c
SenyoGela <senogelamariconb@gmail.com>
2022-12-13 23:02:14 +0800

    add search function

--- graph.py
+++ graph.py
@@ -114,3 +114,8 @@ class City(NamedTuple):
 
     def depth_first_search(graph, source, predicate, order_by=None):
         return search(depth_first_traverse, graph, source, predicate, order_by)
+
+    def search(traverse, graph, source, predicate, order_by=None):
+        for node in traverse(graph, source, order_by):
+            if predicate(node):
+                return node
