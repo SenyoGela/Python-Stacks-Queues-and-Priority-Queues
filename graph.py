7159a1f39b251074167e4d808a6e36c25f506b7b
SenyoGela <senogelamariconb@gmail.com>
2022-12-13 22:02:55 +0800

    add breadth-first traversal method

--- graph.py
+++ graph.py
@@ -33,3 +33,12 @@ class City(NamedTuple):
             for name1, name2, weights in graph.edges(data=True)
         )
 
+    def breadth_first_traverse(graph, source):
+        queue = Queue(source)
+        visited = {source}
+        while queue:
+            yield (node := queue.dequeue())
+            for neighbor in graph.neighbors(node):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.enqueue(neighbor)
