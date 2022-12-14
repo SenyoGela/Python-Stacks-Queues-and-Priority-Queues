d618711154b6090641a11c2c6e900fd3484cfaae
SenyoGela <senogelamariconb@gmail.com>
2022-12-14 16:19:18 +0800

    create async main function

--- async_queues.py
+++ async_queues.py
@@ -5,3 +5,12 @@ from collections import Counter
 
 import aiohttp
 
+async def main(args):
+    session = aiohttp.ClientSession()
+    try:
+        links = Counter()
+        display(links)
+    finally:
+        await session.close()
+
+
