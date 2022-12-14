74d45bcebe0ddd0d870ad0d932214e8dab7286f3
SenyoGela <senogelamariconb@gmail.com>
2022-12-14 22:37:23 +0800

    import modules

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -7,6 +7,8 @@ from string import ascii_lowercase
 import multiprocessing
 from dataclasses import dataclass
 import argparse
+import queue
+import time
 
 # Reversing an MD5 Hash on a Single Thread
 def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
