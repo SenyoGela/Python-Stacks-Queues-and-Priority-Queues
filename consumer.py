a5d803c9883fb022edc99692f79bb45e97dae7d6
SenyoGela <senogelamariconb@gmail.com>
2022-12-15 00:15:11 +0800

    let consumer read sent message

--- consumer.py
+++ consumer.py
@@ -20,3 +20,7 @@ with pika.BlockingConnection() as connection:
     channel.start_consuming()
 
 # Apache Kafka: kafka-python3
+consumer = KafkaConsumer("datascience")
+for record in consumer:
+    message = record.value.decode("utf-8")
+    print(f"Got message: {message}")
